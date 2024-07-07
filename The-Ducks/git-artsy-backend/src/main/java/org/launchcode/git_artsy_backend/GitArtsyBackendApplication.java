87805ebe83bde411a0246c00a48bc0844699aac1
Lorri Lanig <137334064+lorrilanig@users.noreply.github.com>
2024-07-07 01:54:00 -0500

    Add files via upload

---
+++ The-Ducks/git-artsy-backend/src/main/java/org/launchcode/git_artsy_backend/GitArtsyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.git_artsy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitArtsyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitArtsyBackendApplication.class, args);
+	}
+
+}
