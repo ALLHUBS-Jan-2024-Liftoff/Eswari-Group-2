a6a0af851557747b13492cf87378472ddcc64e78
wisevaishu <vpm.win@gmail.com>
2024-07-15 21:31:42 -0500

    Login Landing Page Updated various files in the git-artsy-ui folder

---
+++ The-Ducks/git-artsy-ui/src/components/Login.jsx
@@ -0,0 +1,33 @@
+
+import React, { useState } from 'react';
+import '../cssforpages/Login.css'
+
+const Login = () => {
+    return ( 
+        <div className="container">
+            <div className="header">
+                <div className="text">Login</div>
+                <div className="underline"></div>
+            </div>
+            <form>
+                <div className="inputs">
+                    <div className="input">
+                        <input type="email" name="email" placeholder="Email Address" required />
+                    </div>   
+                    <br></br> 
+                    <div className="input">
+                        <input type="password" name="password" placeholder="Password" required />
+                    </div>
+                </div>
+                <div className="submit-container">
+                    <button type="submit" className="submit">Login</button>
+                </div>
+            </form>
+            <div className="footer">
+                Not a member? <a>Sign up here!</a>
+            </div>
+        </div>
+    )
+};
+
+export default Login;
---
+++ The-Ducks/git-artsy-ui/src/components/LoginLanding.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Login from './Login';
+import ArtworksPopup from './Artwork';
+
+const LandingLogin = () => {
+  return (
+    <div className="app-container">
+        {/* Header Part Coding */}
+      <div className="login-container">
+         <Login /> 
+      </div>
+      <div className="popup-container">
+        <ArtworksPopup />
+      </div>
+      {/* Footer Part */}
+    </div>
+  );
+};
+
+export default LandingLogin;
