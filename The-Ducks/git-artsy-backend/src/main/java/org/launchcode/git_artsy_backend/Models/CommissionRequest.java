8389b42b2c704d243114a98ade6ac20ad402d087
stacijbrooks <stacibrooksfitness@gmail.com>
2024-07-21 11:37:19 -0700

    Began CommissionRequest Model

---
+++ The-Ducks/git-artsy-backend/src/main/java/org/launchcode/git_artsy_backend/Models/CommissionRequest.java
@@ -0,0 +1,57 @@
+package org.launchcode.git_artsy_backend;
+
+
+/**
+ * Represents a commission request in the application.
+ */
+
+public class CommissionRequest {
+
+    private Long id;
+
+    private String detail;
+    private String description;
+    private String subject;
+
+    // Constructors
+    public CommissionRequest() {}
+
+    public CommissionRequest(String detail, String description, String subject) {
+        this.detail = detail;
+        this.description = description;
+        this.subject = subject;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
