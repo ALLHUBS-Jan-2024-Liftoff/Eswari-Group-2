669d49f02be49fec27327cf7f5b31c379a6ce908
stacijbrooks <stacibrooksfitness@gmail.com>
2024-07-22 15:14:32 -0700

    Update Tag and Artwork

--- The-Ducks/git-artsy-backend/src/main/java/org/launchcode/git_artsy_backend/Repo/TagRepository.java
+++ The-Ducks/git-artsy-backend/src/main/java/org/launchcode/git_artsy_backend/Repo/TagRepository.java
@@ -1,5 +1,7 @@
 package org.launchcode.git_artsy_backend.Repo;
 import org.launchcode.git_artsy_backend.Models.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 
  // Repository interface for Tag entities.
