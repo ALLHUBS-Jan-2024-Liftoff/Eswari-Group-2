84ba8b32f772c0f1b286d03cd697b064a8a11ac6
Lorri Lanig <lorrilanig.art@gmail.com>
2024-07-23 14:03:32 -0500

    routers added to main.jsx

--- The-Ducks/git-artsy-ui/src/main.jsx
+++ The-Ducks/git-artsy-ui/src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingLogin from './components/LoginLandingPage/LoginLanding.jsx';
+import SignUpPage from './components/SignUpPage/SignUpPage.jsx';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingLogin />
+  },
+  {
+    path:'/signup',
+    element: <SignUpPage />
+  }
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
