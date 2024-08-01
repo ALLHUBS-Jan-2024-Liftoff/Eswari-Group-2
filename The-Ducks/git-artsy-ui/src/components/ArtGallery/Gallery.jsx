8dc7375ef5aa78dac0d22984ca9caece24a95aba
Lorri Lanig <lorrilanig.art@gmail.com>
2024-08-01 15:33:37 -0500

    headers added

--- The-Ducks/git-artsy-ui/src/components/ArtGallery/Gallery.jsx
+++ The-Ducks/git-artsy-ui/src/components/ArtGallery/Gallery.jsx
@@ -7,7 +7,13 @@ const Gallery = () => {
     return(
         <div>
             <Banner />
+            <div>
+                <h2>From the Masters:</h2>
+            </div>
             <ArtGallery />
+            <div>
+                <h3>From your Fellow Artist:</h3>
+            </div>
         </div>
     )
 }
