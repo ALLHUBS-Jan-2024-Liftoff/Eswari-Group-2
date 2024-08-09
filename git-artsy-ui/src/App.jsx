e6cb47dc435480d5cd6d954eadff2c24f26266d1
stacijbrooks <stacibrooksfitness@gmail.com>
2024-08-08 21:03:59 -0500

    Update App.jsx

--- git-artsy-ui/src/App.jsx
+++ git-artsy-ui/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import PatronCommissionRequest from './components/PatronCommissionRequest/PatronCommissionRequest.jsx';
 
 import LandingLogin from './components/LoginLandingPage/LoginLanding';
 
