2ce5e3f76959f0791997e09a6a4adbd4a8526d27
stacijbrooks <stacibrooksfitness@gmail.com>
2024-08-15 19:11:27 -0500

    Bootstrap

--- git-artsy-ui/src/components/SearchPage/ImageSearch.jsx
+++ git-artsy-ui/src/components/SearchPage/ImageSearch.jsx
@@ -1,23 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './artworksSearch.css';
-import artworksItem from './artworksItem';
-
+import 
 
 // artworksSearch component handles the search functionality for artworkss
 const artworksSearch = () => {
   // State variables for keyword and artworkss
   const [keyword, setKeyword] = useState('');
-  const [artworks, setartworks] = useState([]);
+  const [artworks, setArtworks] = useState([]);
 
   // Handles the search form submission
   const handleSearch = async (e) => {
     e.preventDefault();
     try {
       // Fetch artworkss from the backend API based on the keyword
-      const response = await axios.get(`/api/artworks/search?keyword=${keyword}`);
+      const response = await axios.get(`http://localhost:8082/api/artworks/search?keyword=${tag}`);
       // Update the artworkss state with the fetched data
-      setartworkss(response.data);
+      setArtworks(response.data);
     } catch (error) {
       console.error('Error fetching artworks:', error);
     }
@@ -30,13 +28,13 @@ const artworksSearch = () => {
           type="text"
           value={keyword}
           onChange={(e) => setKeyword(e.target.value)}
-          placeholder="Search for artworkss..."
+          placeholder="Search for artworks..."
         />
         <button type="submit">Search</button>
       </form>
       <div className="artworks-list">
-        {artworkss.map((artworks) => (
-          <artworksItem key={artworks.id} artworks={artworks} />
+        {artworks.map((artwork, index) => (
+          <img key={artwork.fileDownloadUri} alt= {artwork.title} />
         ))}
       </div>
     </div>
