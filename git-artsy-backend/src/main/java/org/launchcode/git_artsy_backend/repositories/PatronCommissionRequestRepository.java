13559ac077199758d4c3de7f444ac70d8493a044
stacijbrooks <stacibrooksfitness@gmail.com>
2024-08-16 20:54:52 -0500

    JPA Repository update n patron commissions repo

--- git-artsy-backend/src/main/java/org/launchcode/git_artsy_backend/repositories/PatronCommissionRequestRepository.java
+++ git-artsy-backend/src/main/java/org/launchcode/git_artsy_backend/repositories/PatronCommissionRequestRepository.java
@@ -1,12 +1,12 @@
 package org.launchcode.git_artsy_backend.repositories;
 
 import org.launchcode.git_artsy_backend.models.PatronCommissionRequest;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 //Repository interface for CommissionRequest entities.
 
 @Repository
-public interface PatronCommissionRequestRepository extends CrudRepository<PatronCommissionRequest, Long> {
+public interface PatronCommissionRequestRepository extends JpaRepository<PatronCommissionRequest, Long> {
 }
