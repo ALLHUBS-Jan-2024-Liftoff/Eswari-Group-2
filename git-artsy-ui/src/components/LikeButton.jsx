792924c19a152d6dd88e256617a3bc8bdf9b4b02
Lorri Lanig <lorrilanig.art@gmail.com>
2024-08-19 20:20:15 -0500

    debug to save and fetch like count

--- git-artsy-ui/src/components/LikeButton.jsx
+++ git-artsy-ui/src/components/LikeButton.jsx
@@ -4,17 +4,48 @@ import Button from "react-bootstrap/Button"
 
 const likeButton = () => {
     //state
+    const [like, setLike] = useState();
+    const [currentLike, setCurrentLike] = useState(null);
     const [count, setCount] = useState(0);
+    
+    //fetch Likes from server
+    useEffect(() => {
+        const fetchLikes = async () => {
+            try {
+                const response = await api.getAllLikes();
+                console.log(response);
+                setLike(response);
+            } catch (error) {
+                console.error('Error in fetching likes')
+            }
+        };
+        fetchLikes();
+    }, []); //runs once
 
-    const handleClick = () => {
-        setCount(count + 1); 
-    }
+
+    const handleChange = async (e) => {
+        e.preventDefault();
+        try {
+            if (currentLike) {
+                await api.updateLike(currentLike, count);
+
+            } else {
+                await api.createLike(count);
+                alert('Liked!');
+            }
+            setCount(count + 1); 
+        } catch (error) {
+            console.error('Error in liking artwork');
+        }
+        
+
+    };
     //
 
 
     return(
         <>
-        <Button onClick={handleClick}>Like</Button>
+        <Button onClick={handleChange}>Like</Button>
         <p>{count}</p>
         </>
     )
